458e8f454453a6aea9b629f256e25c83dd19d4e1
Mohamed Reda Ali <m07hamedreda25@gmail.com>
2025-12-26 19:50:35 +0200

    Nested Routes & env Variables

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from routes import base
+
+load_dotenv(".env")
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome_message():
-    return {
-        "message": "Welcome to the FastAPI application!"
-        }
-
+app.include_router(base.baseRouter)
